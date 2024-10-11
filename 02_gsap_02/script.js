d5a544bdc05b7e371857ccedbf30cb7dd68dc77e
Farooque Sajjad <22sw040@students.muet.edu.pk>
2024-10-11 10:07:42 -0700

    Create script.js

---
+++ 02_gsap_02/script.js
@@ -0,0 +1,18 @@
+// gsap.from("h1",{
+//     // color: "red",
+//     opacity: 0,
+//     y:30,
+//     duration: 3,
+//     delay: 2,
+//     stagger:-1
+
+//
+
+gsap.to("#box", {
+    x: 1200,
+    duration: 1,
+    delay: 1,
+    rotate: 360,
+    repeat: 4,
+     yoyo:true,
+})
